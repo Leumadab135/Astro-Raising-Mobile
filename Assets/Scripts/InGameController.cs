7b85933ade5aad541f4f1d09ac5e0a9e267b262f
DESKTOP-Q9TB58S\Usuario <samueldavidjim123@gmail.com>
2025-05-13 23:39:34 +0200

    Proyect Finished. URP lighning implemented, some art details

--- Assets/Scripts/InGameController.cs
+++ Assets/Scripts/InGameController.cs
@@ -1,20 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
+using System.Collections;
 
 public class InGameController : MonoBehaviour
 {
     [SerializeField] private Transform _playerTransform;
     [SerializeField] private AudioSource _caveAmbient;
+    [SerializeField] private Light2D _finalFloorLight;
+    [SerializeField] private float _startIntensity = 60f;
+    [SerializeField] private float _endIntensity = 2f;
+    [SerializeField] private float _duration =1.5f;
 
     private void Start()
     {
         Application.targetFrameRate = 60;
+        _finalFloorLight.intensity = _startIntensity;
     }
     void Update()
     {
-        if (_playerTransform.position.y > 279)
+        if (_playerTransform.position.y > 270)
         {
             _caveAmbient.Stop();
+            StartCoroutine(DimLight());
         }
 
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -22,5 +31,19 @@ public class InGameController : MonoBehaviour
 
         if (_playerTransform.position.y > 390)
             SceneManager.LoadScene("Win");
+    }
+
+    IEnumerator DimLight()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < _duration)
+        {
+            _finalFloorLight.intensity = Mathf.Lerp(_startIntensity, _endIntensity, elapsed / _duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        _finalFloorLight.intensity = _endIntensity;
     }
 }
